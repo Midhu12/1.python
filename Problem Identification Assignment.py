16e358e3a5a4e50114dab27534d52b50ce22d886
Midhunya <141467640+Midhu12@users.noreply.github.com>
2023-08-16 15:53:05 +0530

    Add files via upload

---
+++ Problem Identification Assignment.py
@@ -0,0 +1,14 @@
+                                   Employee relief predition
+ 
+                   input                                           output
+  
+  Employee details    Age     Salary   Performance              period of resignation   
+                                                                   (week or month)
+    Location          48      45000       good                     (numeric values)
+        
+        
+                        
+                        Machine Learnig
+        
+                      Supervised learning
+                     classification method 
